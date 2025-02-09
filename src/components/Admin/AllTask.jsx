648233639ff19290175c372722c161ca89198ae7
Priyanka singh <160533231+PRIYANKA22BSC@users.noreply.github.com>
2025-02-09 18:21:10 +0530

    ..

--- src/components/Admin/AllTask.jsx
+++ src/components/Admin/AllTask.jsx
@@ -5,43 +5,33 @@ const AllTask = () => {
   const authData = useContext(AuthContext);
 
   return (
-    <div className="bg-[#1c1c1c] flex flex-col gap-4 rounded-sm" style={{ padding: "10px 20px" }}>
-      <div
-        id="alltask"
-        className="grid grid-cols-5 gap-4 bg-amber-600 text-xl font-bold rounded-sm"
-        style={{ padding: "5px 10px" }}
-      >
-        <div className="!m-auto">Name</div>
-        <div className="!m-auto">New</div>
-        <div className="!m-auto">Active</div>
-        <div className="!m-auto">Failed</div>
-        <div className="!m-auto">Completed</div>
-      </div>
-      <div
-        id="alltask"
-        className="flex flex-col gap-4 justify-center overflow-y-auto"
-      >
-        {!authData
-          ? ""
-          : authData.employees.map((emp, idx) => {
-              return (
-                <div
-                  key={idx}
-                  className=" grid grid-cols-5 gap-4 justify-between h-10 rounded-sm border-amber-50 border-2 text-xl font-bold !px-4"
-                >
-                  <div className="!m-auto">{emp.name}</div>
-                  <div className="text-blue-500 !m-auto">{emp.taskCounts.newtask}</div>
-                  <div className=" text-yellow-500 !m-auto">
-                    {emp.taskCounts.active}
-                  </div>
-                  <div className="  text-red-500 !m-auto">{emp.taskCounts.failed}</div>
-                  <div className="text-emerald-600 !m-auto">
-                    {emp.taskCounts.completed}
-                  </div>
-                </div>
-              );
-            })}
-      </div>
+    <div id="alltask" className="bg-[#1c1c1c]  rounded-sm overflow-y-auto" >
+
+<table className="w-full border border-amber-100 text-center">
+    <thead>
+        <tr className="">
+            <th className="border border-emerald-600 px-5 py-4">Name</th>
+            <th className="border border-emerald-600 px-5 py-2">New</th>
+            <th className="border border-emerald-600 px-5 py-2">Active</th>
+            <th className="border border-emerald-600 px-5 py-2">Failed</th>
+            <th className="border border-emerald-600 px-5 py-2">Completed</th>
+        </tr>
+    </thead>
+    <tbody>
+      {!authData?"":authData.employees.map((ele,idx)=>{
+        return(
+<tr key={idx} >
+            <td className="border border-emerald-600  px-5 py-2">{ele.name}</td>
+            <td className="border border-emerald-600 text-blue-600 px-5 py-2">{ele.taskCounts.newtask}</td>
+            <td className="border border-emerald-600 text-amber-300 px-5 py-2">{ele.taskCounts.active}</td>
+            <td className="border border-emerald-600 text-red-500 px-5 py-2">{ele.taskCounts.failed}</td>
+            <td className="border border-emerald-600 text-emerald-600 px-5 py-2">{ele.taskCounts.completed}</td>
+        </tr>
+        )
+      })}
+        
+    </tbody>
+</table>
     </div>
   );
 };
